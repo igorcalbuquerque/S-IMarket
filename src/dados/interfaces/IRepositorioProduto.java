e708099d5ce109e420a321741ad819e55cc302eb
Igor Albuquerque <igor.cezar@ufrpe.br>
2018-11-30 01:07:35 -0300

    Negócio Produto.

--- src/dados/interfaces/IRepositorioProduto.java
+++ src/dados/interfaces/IRepositorioProduto.java
@@ -5,7 +5,7 @@ import negocio.entidades.Produto;
 public interface IRepositorioProduto {
 
     void adicionarProduto(Produto produto);
-    void removerProduto(int codigo);
+    Produto removerProduto(int codigo);
     Produto buscarProduto(int codigo);
     Produto buscarProduto(String codigoBarra);
 
--- src/negocio/NegocioProduto.java
+++ src/negocio/NegocioProduto.java
@@ -1,4 +1,53 @@
 package negocio;
 
+import dados.RepositorioProduto;
+import dados.interfaces.IRepositorioProduto;
+import negocio.entidades.Produto;
+import negocio.excessoes.ProdutoNaoEncontradoException;
+
 public class NegocioProduto {
+
+    private IRepositorioProduto produtos;
+
+    public NegocioProduto(){
+        this.produtos = new RepositorioProduto();
+    }
+    public Produto buscarProdutoPorCodigo(int codigo) throws ProdutoNaoEncontradoException {
+
+        Produto produto = produtos.buscarProduto(codigo);
+
+        if (produto == null){
+            throw new ProdutoNaoEncontradoException();
+        }
+        else{
+            return produto;
+        }
+    }
+
+    public Produto buscarPorCodigoDeBarras(String codigo) throws ProdutoNaoEncontradoException {
+        Produto produto = produtos.buscarProduto(codigo);
+
+        if(produto == null){
+            throw new ProdutoNaoEncontradoException();
+        }
+        else {
+            return produto;
+        }
+    }
+
+    public void removerPorCodigoInterno(int codigo) throws ProdutoNaoEncontradoException{
+        Produto produto = produtos.removerProduto(codigo);
+        if (produto == null){
+            throw new ProdutoNaoEncontradoException();
+        }
+        else {
+            produtos.removerProduto(codigo);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return produtos.toString();
+    }
 }
