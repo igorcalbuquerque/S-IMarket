dd4161c67980cb84eea863900e7fcb49ac91ba57
SauCesar <sau.cesarlima2013@gmail.com>
2018-12-10 21:39:28 -0300

    Interface de fachada associado implementado.

---
+++ src/fachada/interfaces/IFachadaAssociado.java
@@ -0,0 +1,35 @@
+package fachada.interfaces;
+
+import negocio.entidades.*;
+import negocio.entidades.abstratas.Pessoa;
+import negocio.excessoes.*;
+
+public interface IFachadaAssociado {
+    void adicionarCliente(String nome, String rg, String cpf, Endereco endereco) throws CpfJaExisteException, RgJaExisteException;
+    Pessoa buscarClientePorCpf(String cpf) throws CpfNaoEncontrado;
+    Pessoa buscarcClientePorRg(String cpf) throws RgNaoEncontrado;
+    String buscarClientePorNome(String nome) throws PessoaNaoEncotradaException;
+    void removerClientePorRg(String rg) throws RgNaoEncontrado;
+    void removerClientePorCpf(String cpf) throws CpfNaoEncontrado;
+    void adicionarNotas(int numero, int serie, Data dataEmissao, Data dataPagamento, Fornecedor fornecedor) throws NotaJaExisteException;
+    void removerNota(int numero, int codigoFornecedor) throws NenhumaNotaEcontradaException;
+    String listarNotasPorEntrada(Data data) throws NenhumaNotaEcontradaException,DataInvalidaException;
+    String listarNotasPorEmissao(Data data) throws NenhumaNotaEcontradaException,DataInvalidaException;
+    String listarNotasPorNumero(int numero) throws NenhumaNotaEcontradaException;
+    String listarNotasPorFornecedor(Fornecedor fornecedor, Data dataInicial, Data dataFinal)
+           throws NenhumaNotaEcontradaException,DataInvalidaException;
+    NotaFiscal buscarNota(int numero, int codigoFornecedor) throws NotaNaoEncontradaException;
+    void adicionarProduto(int codigo,String codigoBarra,String descricao, double valorCompra, double valorVenda, int codigoSecao)
+           throws CodigoJaExisteException;
+    Produto buscarProdutoPorCodigo(int codigo) throws ProdutoNaoEncontradoException;
+    Produto buscarProdutoPorCodigoDeBarras(String codigo) throws ProdutoNaoEncontradoException;
+    void removerProdutoPorCodigoInterno(int codigo) throws ProdutoNaoEncontradoException;
+    void adicionarVenda(Venda venda);
+    void removerVenda(int codigo) throws VendaNaoEncotradaException;
+    Venda buscarVenda(int codigo) throws VendaNaoEncotradaException;
+    String relatorioVenda(Data dataInicial, Data dataFinal) throws DataInvalidaException, RelatorioVazioException;
+    String relatorioVenda(Cliente cliente, Data dataInicial, Data dataFinal) throws DataInvalidaException,RelatorioVazioException;
+    String relatorioVenda(Funcionario funcionario, Data dataInicial, Data dataFinal) throws DataInvalidaException,RelatorioVazioException;
+    String relatorioVenda(Secao secao, Data dataInicial, Data dataFinal) throws DataInvalidaException,RelatorioVazioException;
+    String relatorioVenda(Produto produto, Data dataInicial, Data dataFinal) throws DataInvalidaException,RelatorioVazioException;
+}
